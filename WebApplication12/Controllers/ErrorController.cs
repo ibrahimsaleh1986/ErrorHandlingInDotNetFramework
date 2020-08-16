e8b2ce6f89ba4384d4041f5964a7205ba7350553
ibrahim saleh <isaleh@tetco.sa>
2020-08-16 13:21:14 +0300

    first commit

---
+++ WebApplication12/Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication12.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CustomErrorPage()
+        {
+            return View();
+        }
+        public ActionResult ApplicationErrorPage()
+        {
+            var x = Server.GetLastError();
+            return View();
+        }
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+        public ActionResult Forbidden()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication12/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApplication12
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            HttpException httpException = exception as HttpException;
+
+            Response.Clear();
+            Server.ClearError();
+
+            if (httpException == null)
+            {
+                Response.Redirect("~/Error/Index");
+            }
+            else
+            {
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        Response.Redirect("~/Error/PageNotFound");
+                        break;
+                    case 500:
+                        Response.Redirect("~/Error/ApplicationErrorPage");
+                        break;
+                    case 403:
+                        Response.Redirect("~/Error/Forbidden");
+                        break;
+
+                }
+            }
+
+            
+        }
+    }
+}
